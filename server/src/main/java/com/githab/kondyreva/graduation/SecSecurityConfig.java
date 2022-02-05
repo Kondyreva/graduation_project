6f1b7e201a8ccd4a829eb5d3e143787f6439fd64
Kondyreva <Kondyreva.T.Vi@sberbank.ru>
2022-02-05 22:09:00 +0300

    security config change

--- server/src/main/java/com/githab/kondyreva/graduation/SecSecurityConfig.java
+++ server/src/main/java/com/githab/kondyreva/graduation/SecSecurityConfig.java
@@ -2,6 +2,7 @@ package com.githab.kondyreva.graduation;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,8 +27,10 @@ public class SecSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(final HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .authorizeRequests().anyRequest().authenticated()
-                .and().httpBasic();
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/server/**").hasRole(MY_ROLE)
+                .and().httpBasic()
+                .and().sessionManagement().disable();
     }
 
     @Bean
