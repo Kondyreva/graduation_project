0683d33f17eb4e7bb9d62f6fc695490b8264db8e
Kondyreva <Kondyreva.T.Vi@sberbank.ru>
2022-02-07 09:14:29 +0300

    trying to pass params

--- client/src/main/java/com/github/kondyreva/graduation/ClientController.java
+++ client/src/main/java/com/github/kondyreva/graduation/ClientController.java
@@ -4,29 +4,47 @@ import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import lombok.extern.java.Log;
 
 @RestController
 @RequestMapping("/balance")
+@Log
 public class ClientController {
     private static final String RESOURCE_URL = "http://localhost:8080/server/getinfo";
     private static final String MY_PASSWORD = "123";
     private static final String MY_LOGIN = "admin";
 
-    @GetMapping("/get")
-    public ResponseEntity<String> get() {
-
+    @GetMapping("/card_number/{cardNumber}/pin/{pin}")
+    public ResponseEntity<String> get(@PathVariable("cardNumber") Integer cardNumber,
+                                      @PathVariable("pin") Integer pin) {
+        log.info("card_number " + cardNumber + " pin " + pin);
         RestTemplate restTemplate = new RestTemplate();
 
+        Map<String, String> body = new HashMap<>();
+        body.put("cardNumber", cardNumber.toString());
+        body.put("pin", pin.toString());
+
+        HttpHeaders requestHeaders = createHeaders(MY_LOGIN, MY_PASSWORD); /*new HttpEntity<String>(createHeaders(MY_LOGIN, MY_PASSWORD))*/
+        requestHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+       /* requestHeaders.add("cardNumber", cardNumber.toString() );
+        requestHeaders.add("pin", pin.toString() );*/
+
+        HttpEntity<Map<String, String>> httpEntity = new HttpEntity<>(body, requestHeaders);
+
         ResponseEntity<String> response =
-                restTemplate.exchange(RESOURCE_URL, HttpMethod.GET, new HttpEntity<String>(createHeaders(MY_LOGIN, MY_PASSWORD)), String.class);
+                restTemplate.exchange(RESOURCE_URL, HttpMethod.POST, httpEntity, String.class);
 
         if (HttpStatus.OK.equals(response.getStatusCode())) {
             return new ResponseEntity<>(response.getBody(), HttpStatus.OK);
         }
 
-        return new ResponseEntity<>("No balance :-(", HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>("No money - no honey :-(", HttpStatus.NO_CONTENT);
     }
 
     HttpHeaders createHeaders(String username, String password) {
--- server/src/main/java/com/githab/kondyreva/graduation/ServerController.java
+++ server/src/main/java/com/githab/kondyreva/graduation/ServerController.java
@@ -4,16 +4,17 @@ import com.githab.kondyreva.graduation.service.AtmTransaction;
 import com.githab.kondyreva.graduation.service.CardTransaction;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import lombok.extern.java.Log;
 
 @RestController
 @RequestMapping("/server")
+@Log
 public class ServerController {
 
-    @GetMapping("/getinfo")
-    public ResponseEntity<String> getInfo() {
+    @PostMapping("/getinfo")
+    public ResponseEntity<String> getInfo(@RequestHeader("cardNumber") Integer cardNumber, @RequestHeader("pin") Integer pin) {
+        log.info("cardNumber=" + cardNumber.toString() + ", pin=" + pin);
         CardTransaction card = new AtmTransaction();
 
         return new ResponseEntity<>(card.getBalance("123","123"), HttpStatus.OK);
